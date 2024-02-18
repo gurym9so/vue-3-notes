cf350d5e5e0cc32373ad21e72d552c54e9c2fec9
gurym9so <oplo.domo@bk.ru>
2024-02-18 22:57:57 +0300

    create form component and redact about page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,15 @@
 <template>
-  <p>HOME</p>
+  <Form @onSubmit="handleSubmit" />
 </template>
+
+<script>
+import Form from "@/components/Notes/Form.vue";
+export default {
+  components: { Form },
+  methods: {
+    handleSubmit(note) {
+      console.log(note);
+    },
+  },
+};
+</script>
